69b5229084d47daace4ffa7b46b04e155d5f2159
billytrinh <quanghoa.trinh@gmail.com>
2023-08-14 19:49:22 +0700

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
-import { ClassRoomComponent } from './classroom/classroom.component';
-import { TeacherComponent } from './teacher/teacher.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent, StudentComponent,
-    ClassRoomComponent,TeacherComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
